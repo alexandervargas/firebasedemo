d984e8eeb0c66686b83ff66519bb11a802e3f413
Mesi Rendon <amrendonsa@unal.edu.co>
2017-12-12 12:28:20 -0500

    Initialize app

---
+++ src/components/Uploader/Uploader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Uploader</h1>
+    <form>
+      <div class="form-group">
+        <div class="dropbox text-white">
+          <input type="file" class="form-control-file input-file" id="photoUploader" accept="image/jpeg,image/png">
+          <p>
+          Drop your image here
+          </p>
+        </div>
+      </div>
+    </form>
+    <div class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">75%</div>
+    </div>
+    <div class="alert alert-success" role="alert">
+        This is a success alert—check it out!
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: 'AIzaSyDHBAjWjW9KTEPwlGzXjaoJMMFT58T9vg8',
+    authDomain: 'biblotestapp.firebaseapp.com',
+    databaseURL: 'https://biblotestapp.firebaseio.com',
+    projectId: 'biblotestapp',
+    storageBucket: 'biblotestapp.appspot.com',
+    messagingSenderId: '939860087378'
+  }
